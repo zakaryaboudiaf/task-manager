e0ae8002720cd13eaeb7585d8a5e4586c1175243
zakaryaboudiaf <zakarya.boudiaf.igee@gmail.com>
2021-10-27 10:35:38 +0200

    h

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path")
+
+const app = express();
+
+app.use(express.static('./public'))
+
+app.get("/", (req , res) =>{
+    console.log('logged')
+    res.sendFile(path.join(__dirname+"/public/index.html"));
+
+
+})
+
+app.listen(5000, () => {
+    console.log("the server is listening on port 5000....")
+})
